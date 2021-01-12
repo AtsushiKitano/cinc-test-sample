1f1178a2bfc93a84f772432d695c4cdc14a7195b
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-01-12 09:33:29 +0900

    [mod] プロジェクトを外部読み込みにするよう編集

--- test/commands/sample.rb
+++ test/commands/sample.rb
@@ -1,10 +1,12 @@
 # coding: utf-8
 
+conf = yaml(content: inspec.profile.file('conf.yaml')).params
+
 control 'command-sample' do
   title 'テキストファイルの内容を確認できるか'
   impact 'critical'
 
   describe command('cat ./test/logs/sample.txt') do
-    its('stdout') { should eq 'ca-kitano-study-sandbox' }
+    its('stdout') { should eq conf["pj"] }
   end
 end
