cfe2865cb6a06e097495e096216ee3a1471ef8b0
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-01-12 10:10:21 +0900

    [mod] テストを混合するように修正

---
+++ test/controls/sample.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+conf = yaml(content: inspec.profile.file('conf.yaml')).params
+
+control 'command-sample' do
+  title 'テキストファイルの内容を確認できるか'
+  impact 'critical'
+
+  describe command('cat ./test/no_gcp_lib/logs/sample.txt') do
+    its('stdout') { should eq conf["pj"] }
+  end
+end
