12ffc139a61fdd0c2f69ee939c9de3d838267017
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-01-14 17:18:56 +0900

    [mod] subnetworkのテスト追加

--- test/controls/network.rb
+++ test/controls/network.rb
@@ -8,4 +8,11 @@ control 'network' do
   describe google_compute_network(project: project["id"], name: network["name"]) do
     it { should exist }
   end
+
+  network["subnetworks"].each do |subnet|
+    describe google_compute_subnetwork(project: project["id"], region: subnet["region"], name: subnet["name"]) do
+      it { should exist }
+      its('ip_cidr_range') { should eq subnet["cidr"] }
+    end
+  end
 end
