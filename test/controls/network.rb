fa405ae08cf87a3310517b43148e6f24362e4e08
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-01-14 15:00:40 +0900

    [mod] テストコードの修正

--- test/controls/network.rb
+++ test/controls/network.rb
@@ -1,8 +1,11 @@
+project = attribute('project')
+network = yaml(content: inspec.profile.file('network.yaml'))
+
 control 'network' do
   title 'demo network'
   impact 'critical'
 
-  describe google_compute_network(project: 'ca-kitano-study-sandbox', name: 'default') do
+  describe google_compute_network(project: project["id"], name: network["name"]) do
     it { should exist }
   end
 end
--- test/files/get_project_info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir ./test/logs
-gcloud config get-value core/project | tr -d "\n" > ./test/logs/sample.txt
