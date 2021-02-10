f19d2251750078ff73d09fadc4316872be4a1b2f
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-02-10 17:33:22 +0900

    [mod] for文テストのコードを追加

--- test/no_gcp_lib/scripts/get_project_info.sh
+++ test/no_gcp_lib/scripts/get_project_info.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+tests=(
+    "abc"
+    "def"
+)
+
 mkdir ./test/no_gcp_lib/logs
 gcloud config get-value core/project | tr -d "\n" > ./test/no_gcp_lib/logs/sample.txt
+
+for test in ${tests[@]}; do
+    echo $test
+done
