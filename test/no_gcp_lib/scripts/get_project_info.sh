df48c4f3db92ce9dae81a64de6afa256641ab0d3
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-02-10 17:41:06 +0900

    [mod] 配列の表記方法の変更

--- test/no_gcp_lib/scripts/get_project_info.sh
+++ test/no_gcp_lib/scripts/get_project_info.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-tests=(
-    "abc"
-    "def"
-)
+tests="abc def"
 
 mkdir ./test/no_gcp_lib/logs
 gcloud config get-value core/project | tr -d "\n" > ./test/no_gcp_lib/logs/sample.txt
 
-for test in ${tests[@]}; do
+for test in $(echo $tests | cut -d ' ' -f 2); do
     echo $test
 done
