09f99e4bb6c6a27f44c15ad40ee9bddef99dad8e
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-02-10 17:50:09 +0900

    [mod] trによって配列を生成するように修正

--- test/no_gcp_lib/scripts/get_project_info.sh
+++ test/no_gcp_lib/scripts/get_project_info.sh
@@ -5,6 +5,6 @@ tests="abc def"
 mkdir ./test/no_gcp_lib/logs
 gcloud config get-value core/project | tr -d "\n" > ./test/no_gcp_lib/logs/sample.txt
 
-for test in $(echo $tests | cut -d ' ' -f 1); do
+for test in $(echo $tests | tr ' ' '\n'); do
     echo $test
 done
