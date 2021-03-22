4a10fdc4b2bf5b5abf0ffc2a3e8ebbee89e5d9e2
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-03-22 14:26:10 +0900

    [mod] コードの修正

--- no_lib_test/scripts/main.sh
+++ no_lib_test/scripts/main.sh
@@ -3,7 +3,7 @@
 CONFIGFILES=files
 
 gcloud compute networks describe sample --format=yaml > $CONFIGFILES/vpc_actual.yaml
-gcloud compute networks subnets describe sample --format=yaml > $CONFIGFILES/subnetwork_actual.yaml
+gcloud compute networks subnets describe --region asia-northeast1 sample --format=yaml > $CONFIGFILES/subnetwork_actual.yaml
 gcloud compute firewall-rules describe ingress-sample --format=yaml > $CONFIGFILES/firewall_actual.yaml
-gcloud compute instances describe sample --format=yaml > $CONFIGFILES/instance_actual.yaml
+gcloud compute instances describe --zone=asia-northeast1-b sample --format=yaml > $CONFIGFILES/instance_actual.yaml
 gcloud compute disks describe --zone=asia-northeast1-b sample --format=yaml > $CONFIGFILES/disk_actual.yaml
