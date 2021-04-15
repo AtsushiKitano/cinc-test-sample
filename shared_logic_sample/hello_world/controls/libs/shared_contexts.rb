9557a2d0185a08d0d209f750056b969abddc4b67
AtsushiKitano <aquamarinearia1010@gmail.com>
2021-04-15 09:55:11 +0900

    [mod] itの内容の修正

--- shared_logic_sample/hello_world/controls/libs/shared_contexts.rb
+++ shared_logic_sample/hello_world/controls/libs/shared_contexts.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'json'
 RSpec.shared_context "hello_world_context" do |file_path, expected|
   let(:actual){ (yaml(content: inspec.profile.file(file_path)).params)["name"] }
@@ -12,7 +13,7 @@ end
 RSpec.shared_context "hello_world_command" do |expected_network, project|
   let(:actual){ JSON.parse(command("gcloud compute networks describe --project #{project} #{expected_network} --format json").stdout) }
 
-  it "file contents is expectd at shared_context" do
+  it "gcloudコマンドで#{expected_network}が、#{project}に作成されていることを確認する" do
     expect(actual["name"]).to eq "sample"
   end
 
